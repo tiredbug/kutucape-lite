5d70d142a828871188b86247c772cb87e733ee14
Tiredbug <im.basoro@gmail.com>
2014-07-01 08:39:37 +0700

    Update frontpage.php

--- frontpage.php
+++ frontpage.php
@@ -6,11 +6,73 @@ Template Name: Frontpage Template
 
 <?php get_header(); ?>
     
-<?php if(have_posts()): while(have_posts()): the_post();?>
-  <?php the_content()?>
-  <?php endwhile; ?> 
-  <?php else: ?>
-  <?php wp_redirect(get_bloginfo('siteurl').'/404', 404); exit; ?>
-<?php endif;?>
+<div class="jumbotron text-center">
+  <div class="container">
+    <h1>Hello, world!</h1>			
+    <p>Simple <a href="http://getbootstrap.com">Bootstrap 3</a> Starter Theme For <a href="http://wordpress.org">WordPress</a>. </p>
+    <p class="light-text">This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+    <p><a class="btn btn-default btn-lg btn-outline" role="button"><i class="fa fa-download"></i>&nbsp; Download</a></p>
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <img src="http://www.tiredbug.com/wp-content/uploads/2014/06/powered-by.png" alt="powered-by" class="img-responsive center-block" />
+    </div>
+  </div>
+</div>
+
+<hr class="powered-divider">
+
+<!-- START THE FEATURETTES -->
+<div class="container">
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive" src="http://placehold.it/500x500" alt="Generic placeholder image">
+    </div>
+  </div>
+  <hr class="featurette-divider">
+  <div class="row featurette">
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive" src="http://placehold.it/500x500" alt="Generic placeholder image">
+    </div>
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+  </div>
+  <hr class="featurette-divider">
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive" src="http://placehold.it/500x500" alt="Generic placeholder image">
+    </div>
+  </div>
+  <hr class="featurette-divider">
+</div>
+<!-- /END THE FEATURETTES -->
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-2 text-right">
+      <img src="http://upload.wikimedia.org/wikipedia/en/9/9f/Twitter_bird_logo_2012.svg" height="60" />
+    </div>
+    <div class="col-sm-10">
+      <blockquote>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="text-right">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+      </blockquote>
+    </div>
+  </div>
+</div>
     
 <?php get_footer(); ?>
