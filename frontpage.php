9951608c825ef5bf894c579b15c2c715c6d01117
Tiredbug <im.basoro@gmail.com>
2014-07-01 12:36:01 +0700

    Update frontpage.php

--- frontpage.php
+++ frontpage.php
@@ -8,10 +8,10 @@ Template Name: Frontpage Template
     
 <div class="jumbotron text-center">
   <div class="container">
-    <h1>Hello, world!</h1>			
+    <h1>Kutu Kutu</h1>			
     <p>Simple <a href="http://getbootstrap.com">Bootstrap 3</a> Starter Theme For <a href="http://wordpress.org">WordPress</a>. </p>
     <p class="light-text">This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-    <p><a class="btn btn-default btn-lg btn-download" role="button"><i class="fa fa-download"></i>&nbsp; Download</a></p>
+    <p><a href="https://github.com/tiredbug/kutu-kutu/archive/master.zip" class="btn btn-default btn-lg btn-download" role="button"><i class="fa fa-download"></i>&nbsp;&nbsp; Download</a></p>
   </div>
 </div>
 
@@ -63,12 +63,9 @@ Template Name: Frontpage Template
 
 <div class="container">
   <div class="row">
-    <div class="col-sm-2 text-right">
-      <img src="http://upload.wikimedia.org/wikipedia/en/9/9f/Twitter_bird_logo_2012.svg" height="60" />
-    </div>
-    <div class="col-sm-10">
+    <div class="col-sm-12">
       <blockquote>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <p><i class="fa fa-twitter fa-3x"></i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
       <footer class="text-right">Someone famous in <cite title="Source Title">Source Title</cite></footer>
       </blockquote>
     </div>
