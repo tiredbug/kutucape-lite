515a41bd43bdf3ebc6f39010eaedf87c3b3d01c3
Tiredbug <im.basoro@gmail.com>
2014-07-02 21:39:03 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -46,6 +46,34 @@ function theme_widgets_init() {
 }
 add_action( 'widgets_init', 'theme_widgets_init' );
 
+// Add Enqueues
+function theme_enqueues()
+{
+		wp_register_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css');
+		wp_enqueue_style('bootstrap');
+
+  	wp_register_script('jquery');
+  	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+    wp_enqueue_script('jquery');
+
+  	wp_register_script('modernizr', '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js');
+    wp_enqueue_script('modernizr');
+
+		wp_register_script('html5shiv', '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.min.js';
+    wp_enqueue_script('html5shiv');
+
+  	wp_register_script('respond', '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js';
+    wp_enqueue_script('respond');
+
+  	wp_register_script('bootstrapjs', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js';
+    wp_enqueue_script('bootstrapjs');
+
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+      }
+}
+add_action('wp_enqueue_scripts', 'theme_enqueues', 100);
+
 // Simple WordPress Walker Nav Menu extends for Bootstrap 
 class Bootstrap_Walker_Nav_Menu extends Walker_Nav_Menu {
   function display_element($element, &$children_elements, $max_depth, $depth=0, $args, &$output) {
