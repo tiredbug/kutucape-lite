db71d2f8f9f659f898e75238fc64f0b27bb901a4
Tiredbug <im.basoro@gmail.com>
2014-07-02 22:33:33 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name'); ?> <?php wp_title('•', true, ''); ?></title>
-  <?php wp_head(); ?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php bloginfo('name'); ?> <?php wp_title('•', true, ''); ?></title>
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
