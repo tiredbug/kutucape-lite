7b2dbaf786a69ca2fc4286045c8b3fa2276e5246
Tiredbug <im.basoro@gmail.com>
2014-07-02 21:39:30 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -12,12 +12,6 @@
 
 <?php wp_footer(); ?>
 
-
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <!-- Include all Bootstrap compiled plugins (below), or include individual files as needed -->
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
   <!-- addClass to reply button and data-toogle dropdown for Walker Nav Menu -->
   <script type="text/javascript">
   $j=jQuery.noConflict();
