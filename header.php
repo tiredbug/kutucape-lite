be26af6b8c3aab12c50503b4fcda664f973a115b
Tiredbug <im.basoro@gmail.com>
2014-07-02 21:40:42 +0700

    Update header.php

--- header.php
+++ header.php
@@ -9,26 +9,7 @@
   <!-- Shortcut Icon -->
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon">
 
-  <!-- Bootstrap -->
-  <link rel="stylesheet" id="bootstrap-css"  href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" type="text/css" media="all" />
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" id="font-awesome-css"  href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" type="text/css" media="all" />
-
-  <!-- Costumize CSS -->
-  <link rel="stylesheet" id="style-css"  href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-  
   <?php wp_head(); ?>
-  
-  <!-- Costumize CSS -->
-  <link rel="stylesheet" id="style-css"  href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
   
 </head>
 
