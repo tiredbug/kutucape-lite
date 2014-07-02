a2feefdd4afa0731759c7e0d72e97c85e584c365
Tiredbug <im.basoro@gmail.com>
2014-07-03 01:01:45 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -43,6 +43,22 @@ function remove_recent_comments_style() {
 }
 add_action( 'widgets_init', 'remove_recent_comments_style' );
 
+// Remove the id="" on nav menu items
+// Return 'menu-slug' for nav menu classes
+function theme_nav_menu_css_class($classes, $item) {
+  $slug = sanitize_title($item->title);
+  $classes = preg_replace('/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes);
+  $classes = preg_replace('/^((menu|page)[-_\w+]+)+/', '', $classes);
+
+  $classes[] = 'menu-' . $slug;
+
+  $classes = array_unique($classes);
+
+  return array_filter($classes, 'is_element_empty');
+}
+add_filter('nav_menu_css_class', 'theme_nav_menu_css_class', 10, 2);
+add_filter('nav_menu_item_id', '__return_null');
+
 // Add Post Thumbnails Support
 add_theme_support('post-thumbnails');
 
