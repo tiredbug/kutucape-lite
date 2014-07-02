9c7f054412d288f5c0b617c8fe587031d5fcba2f
Tiredbug <im.basoro@gmail.com>
2014-07-02 22:31:49 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -59,7 +59,7 @@ add_action( 'widgets_init', 'theme_widgets_init' );
 
 // Add favicon 
 function blog_favicon() { ?>
-  <link rel="shortcut icon" href="<?php echo bloginfo('stylesheet_directory') ?>/favicon.ico" >
+<link rel="shortcut icon" href="<?php echo bloginfo('stylesheet_directory') ?>/favicon.ico" >
 <?php }
 add_action('wp_head', 'blog_favicon');
 
