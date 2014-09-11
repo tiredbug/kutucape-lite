1d09bb059e001a132982f0ed44696bf98e8f27c1
Tiredbug <im.basoro@gmail.com>
2014-09-12 00:20:44 +0700

    Update single.php

--- single.php
+++ single.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 
+<div class="container">
   <div class="row row-offcanvas row-offcanvas-right">
-    
+  
     <div class="col-xs-12 col-sm-8">
       <p class="pull-right visible-xs">
         <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
