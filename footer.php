6255b0046ef7663e55d25a5fe8c5f4b3e3d3b8e7
Tiredbug <im.basoro@gmail.com>
2014-09-13 12:35:29 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -21,29 +21,6 @@
       $j('.row-offcanvas').toggleClass('active')
     });
   });
-  $j.get("http://api.bootswatch.com/3/", function (data) {
-    var themes = data.themes;
-    var select = $j("select");
-    select.show();
-    $j(".alert").toggleClass("alert-info alert-success");
-    $j(".alert h4").text("Success!");
-  
-    themes.forEach(function(value, index){
-      select.append($j("<option />")
-            .val(index)
-            .text(value.name));
-    });
-  
-    select.change(function(){
-      var theme = themes[$j(this).val()];
-      $j("link").attr("href", theme.css);
-    }).change();
-
-  }, "json").fail(function(){
-      $j(".alert").toggleClass("alert-info alert-danger");
-      $j(".alert h4").text("Failure!");
-  });
-</script>
 
 </body>
 </html>
