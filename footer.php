e6944c7e28bc28486a217584083335e4f923360c
Tiredbug <im.basoro@gmail.com>
2014-09-13 12:26:24 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>.</p>
+          <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>. <select></select></p>
         </div>
       </div>
     </div>
