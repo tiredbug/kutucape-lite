8696024a835ce4266b4e6051c5db118110adafa5
Tiredbug <im.basoro@gmail.com>
2014-09-14 11:53:42 +0700

    Update page-canvas.php

--- page-canvas.php
+++ page-canvas.php
@@ -6,14 +6,8 @@ Template Name: Page Canvas
 
 <?php get_header(); ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12 col-sm-12">
-      <?php if(have_posts()): while(have_posts()): the_post();?>
-        <?php the_content()?>
-      <?php endwhile; else: endif;?>
-    </div>
-  </div><!-- .row -->
-</div><!-- .container -->
+  <?php if(have_posts()): while(have_posts()): the_post();?>
+    <?php the_content()?>
+  <?php endwhile; else: endif;?>
 
 <?php get_footer(); ?>
