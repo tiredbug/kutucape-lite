78b2496dcc691af4bf0acf5ce4b14e631dba6253
Tiredbug <im.basoro@gmail.com>
2014-09-14 10:58:58 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
 <hr/>
-  <div class="footer">
-    <div class="container">
-      <div class="row">
         <div class="col-lg-12">
           <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>.</p>
         </div>
-      </div>
-    </div>
-  </div>
+
+  </div><!-- .row -->
+</div><!-- .container -->
 
 <?php wp_footer(); ?>
 
