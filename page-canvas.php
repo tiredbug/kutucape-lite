a8090c1950e1931bebacd0f0a86e423f3760ce48
Tiredbug <im.basoro@gmail.com>
2014-09-14 11:39:39 +0700

    Create page-canvas.php

---
+++ page-canvas.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Page Canvas
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8">
+      <?php if(have_posts()): while(have_posts()): the_post();?>
+        <?php the_content()?>
+      <?php endwhile; else: endif;?>
+    </div>
+  </div><!-- .row -->
+</div><!-- .container -->
+
+<?php get_footer(); ?>
