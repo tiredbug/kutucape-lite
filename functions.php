abb9f7c59b2c19de7dd2d85fc520e74421d0c60a
Tiredbug <im.basoro@gmail.com>
2014-10-08 09:22:40 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+// Add page on theme activation and set bootswatch_style automatically
+if (isset($_GET['activated']) && is_admin()){
+  add_action('init', 'bootswatch_style_setup');
+}
+function bootswatch_style_setup(){
+    set_theme_mod( 'bootswatch_style', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' );
+}
+
 // Clean up wp_head()
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
@@ -60,15 +68,12 @@ add_action('wp_head', 'blog_favicon');
 // Add Enqueues
 function theme_enqueues()
 {
-  wp_register_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css');
-  wp_enqueue_style('bootstrap');
-
-  wp_register_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
-  wp_enqueue_style('font-awesome');
-    
   wp_register_style('bootswatch_style', get_theme_mod('bootswatch_style'));
   wp_enqueue_style('bootswatch_style');
   
+  wp_register_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
+  wp_enqueue_style('font-awesome');
+    
   wp_register_style('style', get_template_directory_uri() . '/style.css');
   wp_enqueue_style('style');
 
@@ -102,15 +107,12 @@ add_filter('nav_menu_css_class', 'nav_class_filter', 100, 1);
 add_filter('nav_menu_item_id', '__return_null');
 
 class wp_bootstrap_navwalker extends Walker_Nav_Menu {
-
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
 		$indent = str_repeat( "\t", $depth );
 		$output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
 	}
-
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-
 		if ( strcasecmp( $item->attr_title, 'divider' ) == 0 && $depth === 1 ) {
 			$output .= $indent . '<li role="presentation" class="divider">';
 		} else if ( strcasecmp( $item->title, 'divider') == 0 && $depth === 1 ) {
@@ -120,33 +122,22 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 		} else if ( strcasecmp($item->attr_title, 'disabled' ) == 0 ) {
 			$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
 		} else {
-
 			$class_names = $value = '';
-
 			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 			$classes[] = 'menu-item-' . $item->ID;
-
 			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-
 			if ( $args->has_children )
 				$class_names .= ' dropdown';
-
 			if ( in_array( 'current-menu-item', $classes ) )
 				$class_names .= ' active';
-
 			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
-
 			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
 			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
-
 			$output .= $indent . '<li' . $id . $value . $class_names .'>';
-
 			$atts = array();
 			$atts['title']  = ! empty( $item->title )	? $item->title	: '';
 			$atts['target'] = ! empty( $item->target )	? $item->target	: '';
 			$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
-
-			// If item has_children add atts to a.
 			if ( $args->has_children && $depth === 0 ) {
 				$atts['href']   		= '#';
 				$atts['data-toggle']	= 'dropdown';
@@ -155,9 +146,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 			} else {
 				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
 			}
-
 			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
-
 			$attributes = '';
 			foreach ( $atts as $attr => $value ) {
 				if ( ! empty( $value ) ) {
@@ -165,72 +154,25 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 					$attributes .= ' ' . $attr . '="' . $value . '"';
 				}
 			}
-
 			$item_output = $args->before;
-
 			if ( ! empty( $item->attr_title ) )
 				$item_output .= '<a'. $attributes .'><i class="fa ' . esc_attr( $item->attr_title ) . '"></i>&nbsp;&nbsp;';
 			else
 				$item_output .= '<a'. $attributes .'>';
-
 			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 			$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
 			$item_output .= $args->after;
-
 			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 		}
 	}
-
 	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
         if ( ! $element )
             return;
-
         $id_field = $this->db_fields['id'];
-
-        // Display this element.
         if ( is_object( $args[0] ) )
            $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
-
         parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
     }
-
-	public static function fallback( $args ) {
-		if ( current_user_can( 'manage_options' ) ) {
-
-			extract( $args );
-
-			$fb_output = null;
-
-			if ( $container ) {
-				$fb_output = '<' . $container;
-
-				if ( $container_id )
-					$fb_output .= ' id="' . $container_id . '"';
-
-				if ( $container_class )
-					$fb_output .= ' class="' . $container_class . '"';
-
-				$fb_output .= '>';
-			}
-
-			$fb_output .= '<ul';
-
-			if ( $menu_id )
-				$fb_output .= ' id="' . $menu_id . '"';
-
-			if ( $menu_class )
-				$fb_output .= ' class="' . $menu_class . '"';
-
-			$fb_output .= '>';
-			$fb_output .= '<li><a href="' . admin_url( 'nav-menus.php' ) . '">Add a menu</a></li>';
-			$fb_output .= '</ul>';
-
-			if ( $container )
-				$fb_output .= '</' . $container . '>';
-
-			echo $fb_output;
-		}
-	}
 }
 
 // Bootswatch Costumizer
@@ -241,7 +183,7 @@ function bootswatch_register_theme_customizer( $wp_customize ){
     'Cosmo' => '//netdna.bootstrapcdn.com/bootswatch/3.2.0/cosmo/bootstrap.min.css',
     'Cyborg' => '//netdna.bootstrapcdn.com/bootswatch/3.2.0/cyborg/bootstrap.min.css',
     'Darkly' => '//netdna.bootstrapcdn.com/bootswatch/3.2.0/darkly/bootstrap.min.css',
-    'Default' => '',
+    'Default' => '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css',
     'Flaty' => '//netdna.bootstrapcdn.com/bootswatch/3.2.0/flatly/bootstrap.min.css',
     'Journal' => '//netdna.bootstrapcdn.com/bootswatch/3.2.0/journal/bootstrap.min.css',
     'Lumen' => '//netdna.bootstrapcdn.com/bootswatch/3.2.0/lumen/bootstrap.min.css',
@@ -293,7 +235,7 @@ function navbar_customizer( $wp_customize ) {
 	
 	// add setting for toggle checkbox
 	$wp_customize->add_setting( 'navbar_toggle', array( 
-		'default' => 1 
+		'default' => 0 
 	) );
 	
 	// add control for toggle checkbox
