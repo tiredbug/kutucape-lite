72f0c760c784906de4b8a866626ec3901bff1743
Tiredbug <im.basoro@gmail.com>
2014-10-09 08:04:56 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 <div class="container">
   <div class="row">
     <div class="col-sm-12 footer text-center">
-      <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>.</p>
+      <p>Proudly powered by <a href="http://wordpress.org">WordPress</a> &middot; <a href="http://getbootstrap.com">Twitter Bootstrap</a> &middot; <a href="http://fontawesome.io">Font Awesome</a> &middot; <a href="http://bootswatch.com">Bootswatch</a> &middot; <a href="http://tiredbug.com/kutu-kutu">Kutu Kutu</a></p>
+      <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
     </div>
   </div><!-- .row -->
 </div><!-- .container -->
